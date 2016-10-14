8d74f95f656389389ac046d1c86eced86e395e0f
urielmandujano <uriel.mandujano14@gmail.com>
2016-10-14 14:33:22 -0500

    Started crawling, stubbed out scraper

--- wordwrangler.py
+++ wordwrangler.py
@@ -14,10 +14,15 @@ Things Crawler needs to do:
     - write visited URLs to file
     - write "next" URLs queue to file
 """
-from urlparse import urlparse
+
+import urllib2
+import cPickle as pk
+import urlparse
+import os
 
 class WordWrangler(object):
     def __init__(self, max_pages, init_url):
+        self.max_pages = max_pages
         self.rootname = self._name_from_url(init_url)
         self.words = self._load_dictionary()
         self.old_urls = self._load_old_urls()
@@ -27,7 +32,7 @@ class WordWrangler(object):
         """
         Extracts name from a url in the format www.name.ext
         """
-        netloc = urlparse(url)[1]
+        netloc = urlparse.urlparse(url)[1]
         start = netloc.find('.') + 1
         end = netloc.find('.', start)
         return netloc[start:end]
@@ -37,14 +42,24 @@ class WordWrangler(object):
         Checks if there is a pre-computed dictionary saved somewhere.
         If so, loads. Else, returns empty dict
         """
-        return {}
+        datafile = self.rootname + ".words"
+        if self._file_exists(self.rootname, datafile):
+            return self._unpickle_data(self.rootname, datafile)
+        else:
+            self._make_dir()
+            return {}
 
     def _load_old_urls(self):
         """
         Checks if there is a file containing previously visited
         urls. If so, loads. Else, returns an empty set
         """
-        return set([])
+        datafile = self.rootname + ".old"
+        if self._file_exists(self.rootname, datafile):
+            return self._unpickle_data(self.rootname, datafile)
+        else:
+            self._make_dir()
+            return set([])
 
     def _load_next(self, init_url):
         """
@@ -52,14 +67,66 @@ class WordWrangler(object):
         continue where we left off. If it has not been visited,
         we want to start a search from this new url
         """
-        if init_url in self.old_urls:
-            # Load next list
-            pass
+        datafile = self.rootname + ".old"
+        if init_url in self.old_urls and \
+           self._file_exists(self.rootname, datafile):
+                return self._unpickle_data(self.rootname, datafile)
         else:
             return [init_url]
 
+    def _file_exists(self, directory, filename):
+        if os.path.exists(directory + "/" + filename):
+            return True
+        return False
+
+    def _make_dir(self):
+        if not os.path.exists(self.rootname):
+            os.makedirs(self.rootname)
+
+    def _unpickle_data(self, directory, filename):
+        instream = open(directory + "/" + filename, 'rb')
+        data = pk.load(instream)
+        instream.close()
+        return data
+
+    def _pickle_data(self, data, directory, filename):
+        outsteam = open(directory + "/" + filename, "wb")
+        pk.dump(data, outstream)
+        outstream.close()
+
+    def save_progress(self):
+        # pickle dictionary
+        # pickle next_urls
+        # pickle old_urls
+        return None
+
+    def begin_wrangling(self):
+        """
+        Has the crawler start issuing page requests in a BFS search
+        through self.next_urls
+        """
+        visited = 0
+        while visited < self.max_pages and len(self.next_urls) != 0:
+            curr_url = self.next_urls.pop(0)
+            htmlpage = self._make_url_request(curr_url)
+            print htmlpage
+            pagecontents = self._scrape_html(htmlpage)
+
+    def _make_url_request(self, url):
+        return urllib2.urlopen(url).read()
+
+    def _scrape_html(self, page):
+        """
+        Scrapes an html page and returns a string containing
+        all the words on the page. Any urls found during
+        scraping will be added to self.next_urls
+        """
+        return ""
+
 def main():
-    ww = WordWrangler(5, "https://docs.python.org/2/library/")
+    ww = WordWrangler(5, "https://en.wikipedia.org/wiki/Bogosort")
+    ww.begin_wrangling()
+    ww.save_progress()
 
 if __name__ == '__main__':
     main()
