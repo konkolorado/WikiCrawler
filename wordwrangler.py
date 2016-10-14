96ceb922667dca7bc8670b733151478b43a637ba
urielmandujano <uriel.mandujano14@gmail.com>
2016-10-14 01:06:30 -0500

    Added loader function stubs

--- wordwrangler.py
+++ wordwrangler.py
@@ -14,16 +14,52 @@ Things Crawler needs to do:
     - write visited URLs to file
     - write "next" URLs queue to file
 """
+from urlparse import urlparse
 
 class WordWrangler(object):
     def __init__(self, max_pages, init_url):
-        print "Wrangler created"
+        self.rootname = self._name_from_url(init_url)
+        self.words = self._load_dictionary()
+        self.old_urls = self._load_old_urls()
+        self.next_urls = self._load_next(init_url)
 
+    def _name_from_url(self, url):
+        """
+        Extracts name from a url in the format www.name.ext
+        """
+        netloc = urlparse(url)[1]
+        start = netloc.find('.') + 1
+        end = netloc.find('.', start)
+        return netloc[start:end]
 
+    def _load_dictionary(self):
+        """
+        Checks if there is a pre-computed dictionary saved somewhere.
+        If so, loads. Else, returns empty dict
+        """
+        return {}
 
+    def _load_old_urls(self):
+        """
+        Checks if there is a file containing previously visited
+        urls. If so, loads. Else, returns an empty set
+        """
+        return set([])
+
+    def _load_next(self, init_url):
+        """
+        If init_url has already been visited, we want to
+        continue where we left off. If it has not been visited,
+        we want to start a search from this new url
+        """
+        if init_url in self.old_urls:
+            # Load next list
+            pass
+        else:
+            return [init_url]
 
 def main():
-    ww = WordWrangler(5, "www.wikipedia.org")
+    ww = WordWrangler(5, "https://docs.python.org/2/library/")
 
 if __name__ == '__main__':
     main()
